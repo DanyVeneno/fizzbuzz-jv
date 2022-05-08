bb3b14b27df146854fc8197292525cd617e2d05e
Daniel Vallejo <yehiibhiidigital@gmail.com>
2022-05-08 17:41:18 -0500

    7- retomando el proyecto

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const Reader = require('./../lib/utils/Reader')
 const ExploreService = require('./../lib/services/ExplorerService')
 
+
 const explorers = Reader.readJsonFile("explorers.json")
 
-console.log(explorers)
+//console.log(explorers)
 
-
-// Clase anterior con la que obtenemos los explorers
-const Reader = require("./lib/utils/Reader");
-const explorers = Reader.readJsonFile("explorers.json");
-
-// Aplicación del ExplorerService sobre la lista de explorers
-//ExplorerService.filterByMission(explorers, "node");
-//ExplorerService.getAmountOfExplorersByMission(explorers, "node");
-//ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+console.log(ExplorerService.filterByMission(explorers, "node"));
+console.log(ExplorerService.getAmountOfExplorersByMission(explorers, "node"));
+console.log(ExplorerService.getExplorersUsernamesByMission(explorers, "node"));
--- test/service/ExplorerService.test.js
+++ test/service/ExplorerService.test.js
@@ -1,30 +1,32 @@
-const ExplorerService = require("./../../lib/services/ExplorerService")
-const Reader = require("../lib/utils/Reader")
+const ExplorerService = require ("./../../lib/services/ExplorerService")
+const Reader = require ("./../../lib/utils/Reader")
 
 
 describe("Test para ExplorerService",() => {
+
+    //const explorers = Reader.readJsonFile("./explorers.json")
 
     test("Requerimiento 1: Calcular todos los explorers de la missión LaunchX",() => {
 
     const explorers =[{mission:"node"}];
     const explorersInNode= ExplorerService.filterByMission(explorers,"node");
-    expect(explorersInNode.length).toBe(1);
+    expect(explorersInNode.length).toBe({mission:"node"});
 
     })  
 
-    test("Requerimiento 2:Obtener la cantidad los explorers en la missión NodeJS",() => {
+    /*test("Requerimiento 2:Obtener la cantidad los explorers en la missión NodeJS",() => {
 
     const explorers =[{mission:"node"}];
     const getAmountOfExplorersByMission = explorers.filter((explorer)=> explorer.mission === mission)
-    expect(explorersInNode.length).toBe(1);
+    expect(explorers.length).toBe(10);
     
-        })  
-    test("Requerimiento 3:Obtener todos los nombres de los explorers en la missión NodeJS",() => {
+        })  */
+    /*test("Requerimiento 3:Obtener todos los nombres de los explorers en la missión NodeJS",() => {
 
     const explorers =[{mission:"node"}];
     const explorersInNode= ExplorerService.filterByMission(explorers,"node");
-    expect(explorersInNode.length).toBe(1);
+    expect(explorers.githubusername).toBe(1);
         
-            })  
+            })  */
 
 })
